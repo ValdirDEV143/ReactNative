113ac74c24f1e0c77ea0dc60ac7f146bdd913144
ValdirDEV143 <prof.valdir.escola@gmail.com>
2025-07-21 17:07:08 -0400

    inclusao das imagens

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,53 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+import { FaCartPlus } from "react-icons/fa";
+import { CgAirplane, CgAlarm  } from "react-icons/cg";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-dark-200 font-bold">Bem vindo</Text>
+  <View className="flex-1 flex-row">
+
+<View className="w-40 h-40 ">
+<Image  className="w-1 h-1 flex ml-8 mr-4 "
+ source={require('../assets/images/marrom.png')}
+ />
+<Text className=" font-bold text-red-500">Título: Blusa Marrom</Text>
+<Text className=" font-bold text-blue-600">Descrição: Em algodão</Text>
+<Text className=" font-bold text-green-700 ">$1.000,00</Text>
+</View>
+
+<View className="w-40 h-40 ">
+<Image  className="w-1 h-1 flex ml-8 mr-4 "
+ source={require('../assets/images/rosa.png')}
+ />
+<Text className=" font-bold text-red-500">Título: Blusa Marrom</Text>
+<Text className=" font-bold text-blue-600">Descrição: Em algodão</Text>
+<Text className=" font-bold text-green-700 ">$1.000,00</Text>
+</View>
+
+<View className="w-40 h-40 ">
+<Image  className="w-1 h-1 flex ml-8 mr-4 "
+ source={require('../assets/images/marrom.png')}
+ />
+<Text className=" font-bold text-red-500">Título: Blusa Marrom</Text>
+<Text className=" font-bold text-blue-600">Descrição: Em algodão</Text>
+<Text className=" font-bold text-green-700 ">$1.000,00</Text>
+</View>
+
+
+
+
+<View>
+
+
+
+</View>
+{/* 
+      <Text className="text-5xl text-dark-200 font-bold bg-red-500 p-5 rounded hover:backdrop:first-letter:bg-lime-300 green">Bem vindo ao melhor show de Rock and Roll da sua vida!<CgAirplane /> <CgAlarm /></Text>
+    */}
+   
+   
+   
+   
     </View>
   );
 }
